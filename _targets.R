84dd202a8c9135f88ee1d6028aea60cc0549a3a4
DinaGyberg <vbz248@alumni.ku.dk>
2026-05-12 08:52:41 +0200

    Target file updated

--- _targets.R
+++ _targets.R
@@ -46,3 +46,20 @@ list(
     name = rina_bb_test_plot,
     command = get_rina_bb_test_plot(rina_bb_test_data, B = 10^4, N = 2500, n = 500))
 )
+
+tar_target(
+  name = e_vs_p_power_data,
+  command = get_e_vs_p_power(alphas = c(0.001, 0.01, 0.05),
+                             Bs = c(1000, 5000, 10000),
+                             ns = seq(50, 250, by = 50),
+                             ps = seq(0.1, 0.9, by = 0.1),
+                             n = 100,
+                             B = 5000,
+                             alpha = 0.05,
+                             p = 0.7,
+                             lambda = 0.5))
+
+tar_target(
+  name = e_vs_p_power_plot,
+  command = get_e_vs_p_power_plot(e_vs_p_power_data)
+)
